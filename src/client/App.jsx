8d1c632633b4fcbaecfaef85d55735599a2309c0
ThomasOh92 <ohcsthomas@gmail.com>
2020-06-01 23:15:28 +0800

    Finished MVP, used momentjs

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import moment from 'moment';
 
 class App extends React.Component {
+  constructor(){
+    super()
+  
+    this.state = {
+      toDoArray: [],
+      inputValue: ""
+    }
+  }
+
+  inputHandler(event){
+    this.setState({inputValue: event.target.value})
+  }
+
+  clickHandler(){
+    let updatedItem = {item: this.state.inputValue,
+                       date: moment().format('MMMM Do YYYY, h:mm:ss a')}
+    this.setState({toDoArray:[updatedItem, ...this.state.toDoArray], inputValue: ""});
+  }
+
   render() {
+    let toDoElements = this.state.toDoArray.map(item => <li>{item.item}, {item.date}</li>)
     return (
       <div>
-        Welcome.
+        These are the things to do
+        <ol>{toDoElements}</ol>
+        <input onChange={(event)=>{this.inputHandler(event)}} value={this.state.inputValue}/>
+        <button onClick={() =>{this.clickHandler()}}> Submit </button>
       </div>
     );
   }
